384c58df45a3591d3af64d3340cb678309b571e8
Annalisa Alden <annalisa.alden@gmail.com>
2018-01-18 18:09:14 +0000

    working on animating props

--- react-props/app.js
+++ react-props/app.js
@@ -24,3 +24,28 @@ $(".props").mouseout(function() {
         $(this).removeClass($(this).attr("data-color"))
     })
 })
+
+$("#animateProps").click(function(){
+    $(this).css("display", "none")
+    $("#reset").css("display", "inline")
+    $(".animateHide").css("display", "none")
+    $(".animateShow").css("display", "block")
+    $(".switch1").animate({
+        opacity: 0
+    }, function(){
+        $(".switch1").each(function(){
+            $(this).text($(this).attr("data-switch")).css("color", "#00acc1")
+        })
+        $(".switch1").animate({
+            opacity: 1,
+        })
+    })
+})
+
+$("#reset").click(function(){
+    $(this).css("display", "none")
+    $("#animateProps").css("display", "inline")
+    $(".animateShow").css("display", "none")
+    $(".animateHide").css("display", "block")
+})
+
